4bf66fe46e99c3b50693be25e0c6a100a4c7be19
stokato <demyanovpaul@yandex.ru>
2024-02-06 00:16:49 +0300

    Вывод списка работ и фильтр готов.

--- src/admin.js
+++ src/admin.js
@@ -1,5 +1,6 @@
 import { JobsService } from './modules/JobsService';
 import { auth } from './modules/admin/auth';
+import { items } from './modules/admin/items';
 import { UsersService } from './modules/admin/usersService';
 import { ConfigService } from './modules/configService';
 
@@ -8,3 +9,7 @@ window.jobsService = new JobsService();
 window.config = new ConfigService();
 
 auth();
+
+if (window.location.href.includes('table.html')) {
+  items();  
+}
---
+++ src/modules/admin/items.js
@@ -0,0 +1,86 @@
+import { filter } from "../filter";
+
+export const items = () => {
+  const filterItems = document.getElementById('typeItem');
+
+  const filterView = async () => {
+    const textFilters = await filter();
+
+    filterItems.innerHTML = '<option value="">Все услуги</option>';
+
+    textFilters.forEach(text => {
+      const option = document.createElement('option');
+
+      option.value = text;
+      option.textContent = text;
+
+      filterItems.appendChild(option);
+    });
+  };
+
+  const printTableJobs = (data) => {
+    const itemsTable = document.getElementById('tbody');
+    itemsTable.innerHTML = '';
+
+    data.forEach(job => {
+      const tr = document.createElement('tr');
+      const tdId = document.createElement('td');
+      const tdName = document.createElement('td');
+      const tdType = document.createElement('td');
+      const tdUnits = document.createElement('td');
+      const tdCost = document.createElement('td');
+      const tdActions = document.createElement('td');
+      const divActions = document.createElement('div');
+      const btnChange = document.createElement('button');
+      const btnRemove = document.createElement('button');
+
+      tr.classList.add('table__row');
+      tdId.classList.add('table__id', 'table__cell');
+      tdName.classList.add('table-name', 'table__cell');
+      tdType.classList.add('table-type', 'table__cell');
+      tdUnits.classList.add('table-units', 'table__cell');
+      tdCost.classList.add('table-cost', 'table__cell');
+      divActions.classList.add('table__actions', 'table__cell');
+      btnChange.classList.add('button', 'action-change');
+      btnRemove.classList.add('button', 'action-remove');
+
+      tdId.textContent = job.id;
+      tdType.textContent = job.type;
+      tdName.textContent = job.name;
+      tdUnits.textContent = job.units;
+      tdCost.textContent = `${job.cost} руб`;
+
+      btnChange.innerHTML = '<span class="svg_ui"><svg class="action-icon_change"><use xlink:href="./img/sprite.svg#change"></use></svg></span><span>Изменить</span>';
+      btnRemove.innerHTML = '<span class="svg_ui"><svg class="action-icon_remove"><use xlink:href="./img/sprite.svg#remove"></use></svg></span><span>Удалить</span>';
+
+      divActions.appendChild(btnChange);
+      divActions.appendChild(btnRemove);
+      tdActions.appendChild(divActions);
+
+      tr.appendChild(tdId);
+      tr.appendChild(tdType);
+      tr.appendChild(tdName);
+      tr.appendChild(tdUnits);
+      tr.appendChild(tdCost);
+      tr.appendChild(tdActions);
+
+      itemsTable.appendChild(tr);
+    });
+  };
+
+  const render = () => {
+    filterView();
+    jobsService.getJobs().then(data => printTableJobs(data));
+  };
+
+  filterItems.addEventListener('change', (e) => {
+    if (!e.target.value) {
+      jobsService.getJobs().then(data => printTableJobs(data));
+    } else {
+      jobsService.getFilterTypes(e.target.value).then(data => printTableJobs(data));
+    }
+    
+  });
+
+  render();
+};
